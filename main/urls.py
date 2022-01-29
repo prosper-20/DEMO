28672b749ad02896e10529a82b3681de24e04df2
prosper-20 <edwardprosper001@gmail.com>
2022-01-29 16:36:38 +0100

    updates

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("detail/<slug>", TaskDetailView.as_view(), name="detail"),
     path("create/", TaskCreateView.as_view(), name="create"),
     path("update/<slug>/", TaskUpdateView.as_view(), name="update"),
-    path("user/<str:username>/", UserHomeView, name="user_home")
+    path("user/<str:username>/", UserHomeView.as_view(), name="user_home")
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -9,13 +10,26 @@ from django.views.generic import (
 from .models import Task
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-def UserHomeView(self, request):
-    user = self.request.user
-    task = Task.objects.filter(user=user)
-    context = {
-        "task": task
-    }
-    return render(request, "main/user_tasks.html", context)
+# def UserHomeView(self, request, **kwargs):
+#     user = self.request.user
+#     task = Task.objects.filter(user=user)
+#     username=self.kwargs.get("username")
+#     context = {
+#         "task": task,
+#         "username": username,
+#     }
+#     return render(request, "main/user_tasks.html", context)
+
+
+class UserHomeView(ListView):
+    model = Task
+    template_name = "main/user_tasks.html"
+    context_object_name = "tasks"
+
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get("username"))
+        return Task.objects.filter(user=user)
 
 
 class HomeView(ListView):
