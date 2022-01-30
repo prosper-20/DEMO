8514686714ac4a11f64c22d7a39c4af13dde1430
prosper-20 <edwardprosper001@gmail.com>
2022-01-30 17:38:38 +0100

    updates

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,15 @@
 import email
 from django.contrib.auth.models import User
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
 
-class UserRegisterForm(forms.ModelForm):
+class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(help_text="Enter a valid email address")
 
-    class meta:
+    class Meta:
         model = User
 
-        fields=["username", "email", "password1", "password2"]
+        fields = ["username", "email", "password1", "password2"]
 
         
