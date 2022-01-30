c7e4c15f0538fd305bdba634efe8a3719f72c357
prosper-20 <edwardprosper001@gmail.com>
2022-01-30 17:20:45 +0100

    updates

--- main/views.py
+++ main/views.py
@@ -81,19 +81,7 @@ class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 
 
-def register(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get("username")
-            messages.success(request, "Hi {username}, your account has been created successfully!")
-            return redirect('login')
-    else:
-        form = UserCreationForm()
-    context = {
-        "form": form,
-    }
-    return render(request, "main/register.html", context)
+
 
 
     
